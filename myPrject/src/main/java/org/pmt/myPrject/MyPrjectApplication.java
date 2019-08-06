d86342f5e3057f164530630dc1ea1c635f2383e1
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-08-06 07:04:53 -0400

    first commit

---
+++ myPrject/src/main/java/org/pmt/myPrject/MyPrjectApplication.java
@@ -0,0 +1,13 @@
+package org.pmt.myPrject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPrjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPrjectApplication.class, args);
+	}
+
+}
---
+++ myPrject/src/test/java/org/pmt/myPrject/MyPrjectApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pmt.myPrject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyPrjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
