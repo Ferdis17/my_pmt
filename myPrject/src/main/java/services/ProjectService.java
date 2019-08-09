7a295188d7ad4a8a7afccf566a9edd736b8d5217
Ferdis17 <fmuriyesu@users.noreply.github.com>
2019-08-09 07:34:26 -0400

    repo, controller,service

--- myPrject/src/main/java/services/ProjectService.java
+++ myPrject/src/main/java/services/ProjectService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import domain.Project;
+import exceptions.ProjectIdException;
 import repository.ProjectRepository;
 
 @Service
@@ -15,9 +16,34 @@ public class ProjectService {
 	public Project saveOrUpdateProject(Project project) {
 		
 		// some logic should be added
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		}catch (Exception ex) {
+			throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "doesn't exist");
+		}
+		
 	}
 	
+	public Project findByProjectIdentifier(String projectId) {
+		
+		Project  project = projectRepository.findbyProjectIdentifier(projectId.toUpperCase());
+		 if(project == null) {
+			 throw new ProjectIdException("Project Id' " + projectId + " doesn't exist");
+		 }
+		 return project;
+	}
 	
+	public Iterable<Project>findAllProjects(){
+		return projectRepository.findAll();	
+		}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project = projectRepository.findbyProjectIdentifier(projectId);
+		if(project ==  null) {
+			throw new ProjectIdException("cannot delete project with ID ' " +projectId+"'. This project does not exist");
+		}
+		projectRepository.delete(project);
+	}
 
 }
